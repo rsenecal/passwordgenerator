f54f10f78371397a939bc026b5557fe272df668c
rsenecal <roodysenecal@gmail.com>
2022-09-28 17:14:04 -0400

    Initial Commit: Getting something on the page

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword() {
+ var password = "2345658";
+ return password
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
